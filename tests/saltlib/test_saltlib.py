7a1e05e19a6836dc5d7b77c83392b6ce53875f50
ppmag <mybestexpert@gmail.com>
2017-07-19 00:56:44 +0300

    SaltTestData -> CryptoTestData

--- tests/saltlib/test_saltlib.py
+++ tests/saltlib/test_saltlib.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import os
 import timeit
 from functools import partial
@@ -16,46 +15,6 @@ from saltchannel.saltlib.saltlib import SaltLib, LibType, RngType
 
 from saltchannel.util.crypto_test_data import CryptoTestData
 
-naclapi_map = {
-            '1. SaltLibNative': SaltLibNative(),
-            '2. SaltLibPyNaCl': SaltLibPyNaCl(),
-            '3. SaltLibTweetNaClExt': SaltLibTweetNaClExt(),
-            #'4. SaltLibPure': SaltLibPure(),
-        }
-
-class SaltTestData:
-
-    aSigPub = bytes.fromhex('5529ce8ccf68c0b8ac19d437ab0f5b32723782608e93c6264f184ba152c2357b')
-    aSigSec = bytes.fromhex('55f4d1d198093c84de9ee9a6299e0f6891c2e1d0b369efb592a9e3f169fb0f79') + aSigPub
-
-    aEncSec = bytes([
-        0x77, 0x07, 0x6d, 0x0a, 0x73, 0x18, 0xa5, 0x7d,
-        0x3c, 0x16, 0xc1, 0x72, 0x51, 0xb2, 0x66, 0x45,
-        0xdf, 0x4c, 0x2f, 0x87, 0xeb, 0xc0, 0x99, 0x2a,
-        0xb1, 0x77, 0xfb, 0xa5, 0x1d, 0xb9, 0x2c, 0x2a,
-    ])
-
-    aEncPub = bytes([
-        0x85, 0x20, 0xf0, 0x09, 0x89, 0x30, 0xa7, 0x54,
-        0x74, 0x8b, 0x7d, 0xdc, 0xb4, 0x3e, 0xf7, 0x5a,
-        0x0d, 0xbf, 0x3a, 0x0d, 0x26, 0x38, 0x1a, 0xf4,
-        0xeb, 0xa4, 0xa9, 0x8e, 0xaa, 0x9b, 0x4e, 0x6a,
-    ])
-
-    bEncSec = bytes( [
-        0x5d, 0xab, 0x08, 0x7e, 0x62, 0x4a, 0x8a, 0x4b,
-        0x79, 0xe1, 0x7f, 0x8b, 0x83, 0x80, 0x0e, 0xe6,
-        0x6f, 0x3b, 0xb1, 0x29, 0x26, 0x18, 0xb6, 0xfd,
-        0x1c, 0x2f, 0x8b, 0x27, 0xff, 0x88, 0xe0, 0xeb,
-    ])
-
-    bEncPub =  bytes([
-        0xde, 0x9e, 0xdb, 0x7d, 0x7b, 0x7d, 0xc1, 0xb4,
-        0xd3, 0x5b, 0x61, 0xc2, 0xec, 0xe4, 0x35, 0x37,
-        0x3f, 0x83, 0x43, 0xc8, 0x5b, 0x78, 0x67, 0x4d,
-        0xad, 0xfc, 0x7e, 0x14, 0x6f, 0x88, 0x2b, 0x4f,
-    ])
-
 class BaseTest(TestCase):
     def __init__(self, *args, **kwargs):
         TestCase.__init__(self, *args, **kwargs)
@@ -68,14 +27,21 @@ class BaseTest(TestCase):
 
 
 class TestSaltLib(BaseTest):
+   
+    naclapi_map = {
+        '1. SaltLibNative': SaltLibNative(),
+        '2. SaltLibPyNaCl': SaltLibPyNaCl(),
+        '3. SaltLibTweetNaClExt': SaltLibTweetNaClExt(),
+        # '4. SaltLibPure': SaltLibPure(),
+    }
 
     def test_nacl_api_available(self):
-        for (name, api) in naclapi_map.items():
+        for (name, api) in self.naclapi_map.items():
             with self.subTest(name=name):
                 self.assertTrue(api.isAvailable())
 
     def test_crypto_hash_emptymsg(self):
-        for (name, api) in naclapi_map.items():
+        for (name, api) in self.naclapi_map.items():
             with self.subTest(name=name):
                 self.assertEqual(api.crypto_hash(b''),
                              bytes([
@@ -90,7 +56,7 @@ class TestSaltLib(BaseTest):
                             ]))
 
     def test_crypto_hash_abc(self):
-        for (name, api) in naclapi_map.items():
+        for (name, api) in self.naclapi_map.items():
             with self.subTest(name=name):
                 self.assertEqual(api.crypto_hash(b'abc'),
                          bytes([
@@ -105,30 +71,30 @@ class TestSaltLib(BaseTest):
                          ]))
 
     def test_crypto_sign_keypair_not_random(self):
-        seed = SaltTestData.aSigSec[0:SaltLibBase.crypto_sign_SEEDBYTES]
-        for (name, api) in naclapi_map.items():
+        seed = CryptoTestData.aSig.sec[0:SaltLibBase.crypto_sign_SEEDBYTES]
+        for (name, api) in self.naclapi_map.items():
             with self.subTest(name=name):
                 (pk, sk) = api.crypto_sign_keypair_not_random(seed)
-                self.assertEqual(SaltTestData.aSigPub, pk)
-                self.assertEqual(SaltTestData.aSigSec, sk)
+                self.assertEqual(CryptoTestData.aSig.pub, pk)
+                self.assertEqual(CryptoTestData.aSig.sec, sk)
 
     def test_crypto_box_keypair_not_random(self):
-        sk1 = SaltTestData.aEncSec
-        sk2 = SaltTestData.bEncSec
-        for (name, api) in naclapi_map.items():
+        sk1 = CryptoTestData.aEnc.sec
+        sk2 = CryptoTestData.bEnc.sec
+        for (name, api) in self.naclapi_map.items():
             with self.subTest(name=name):
                 (pk1, sk1) = api.crypto_box_keypair_not_random(sk1)
-                self.assertEqual(SaltTestData.aEncPub, pk1)
-                self.assertEqual(SaltTestData.aEncSec, sk1)
+                self.assertEqual(CryptoTestData.aEnc.pub, pk1)
+                self.assertEqual(CryptoTestData.aEnc.sec, sk1)
                 (pk2, sk2) = api.crypto_box_keypair_not_random(sk2)
-                self.assertEqual(SaltTestData.bEncPub, pk2)
-                self.assertEqual(SaltTestData.bEncSec, sk2)
+                self.assertEqual(CryptoTestData.bEnc.pub, pk2)
+                self.assertEqual(CryptoTestData.bEnc.sec, sk2)
 
     def test_crypto_sign(self):
         m = bytes([3, 3, 3, 3])
-        sk = SaltTestData.aSigSec
-        pk = SaltTestData.aSigPub
-        for (name, api) in naclapi_map.items():
+        sk = CryptoTestData.aSig.sec
+        pk = CryptoTestData.aSig.pub
+        for (name, api) in self.naclapi_map.items():
             with self.subTest(name=name):
                 sm = api.crypto_sign(m, sk)
                 m2 = api.crypto_sign_open(sm, pk)
@@ -140,24 +106,24 @@ class TestSaltLib(BaseTest):
                     api.crypto_sign_open(badsm, pk)
 
     def test_crypto_box_beforenm(self):
-        ask = SaltTestData.aEncSec
-        apk = SaltTestData.aEncPub
-        bsk = SaltTestData.bEncSec
-        bpk = SaltTestData.bEncPub
-        for (name, api) in naclapi_map.items():
+        ask = CryptoTestData.aEnc.sec
+        apk = CryptoTestData.aEnc.pub
+        bsk = CryptoTestData.bEnc.sec
+        bpk = CryptoTestData.bEnc.pub
+        for (name, api) in self.naclapi_map.items():
             with self.subTest(name=name):
                 k1 = api.crypto_box_beforenm(bpk, ask)
                 k2 = api.crypto_box_beforenm(apk, bsk)
                 self.assertEqual(k1, k2)
 
     def test_crypto_box(self):
-        ask = SaltTestData.aEncSec
-        apk = SaltTestData.aEncPub
-        bsk = SaltTestData.bEncSec
-        bpk = SaltTestData.bEncPub
+        ask = CryptoTestData.aEnc.sec
+        apk = CryptoTestData.aEnc.pub
+        bsk = CryptoTestData.bEnc.sec
+        bpk = CryptoTestData.bEnc.pub
         m = b'abcdEFGH'
         n = os.urandom(SaltLibBase.crypto_box_NONCEBYTES)
-        for (name, api) in naclapi_map.items():
+        for (name, api) in self.naclapi_map.items():
             with self.subTest(name=name):
                 k1 = api.crypto_box_beforenm(bpk, ask)
                 k2 = api.crypto_box_beforenm(apk, bsk)
@@ -248,14 +214,14 @@ class BenchSaltLib:
 
     def body_crypto_sign(self):
         m = self.rndmsg
-        sk = SaltTestData.aSigSec
-        pk = SaltTestData.aSigPub
+        sk = CryptoTestData.aSig.sec
+        pk = CryptoTestData.aSig.pub
         sm = self.api.crypto_sign(m, sk)
         m2 = self.api.crypto_sign_open(sm, pk)
 
     def body_crypto_box(self):
-        ask = SaltTestData.aEncSec
-        bpk = SaltTestData.bEncPub
+        ask = CryptoTestData.aEnc.sec
+        bpk = CryptoTestData.bEnc.pub
         m = b'\0' * SaltLibBase.crypto_box_BOXZEROBYTES + self.rndmsg
 
         k1 = self.api.crypto_box_beforenm(bpk, ask)
@@ -281,7 +247,7 @@ class BenchSaltLib:
 
 
     def run_bench_suite(self):
-        for apiname, api in sorted(naclapi_map.items()):
+        for apiname, api in sorted(self.naclapi_map.items()):
             print("="*79, "\n {}\n".format(api.__class__.__name__), "-"*78, "\n")
             self.set_api(api)
             for mtd in [method for method in dir(self)
