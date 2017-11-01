22cb4c058e3b82ba456a2abec9c3508f35a05cb9
ppmag <mybestexpert@gmail.com>
2017-11-01 13:49:41 +0200

    basic A1/A2 unittests added

--- tests/a1a2/test_packets.py
+++ tests/a1a2/test_packets.py
@@ -107,6 +107,45 @@ class TestA2(BaseTest):
         self.assertEqual(bytes(a2c.Prot[0].P1), A2Packet.SC2_PROT_STRING)
         self.assertEqual(bytes(a2c.Prot[0].P2), A2Packet.UNSPECIFIED_PROT_STRING)
 
+    def test_A2_prop_valid3(self):
+        P1_TEST_VALID = b'A01b_3-de_'   # valid ranges: 'A' - 'Z', 'a' - 'z', '0' - '9', '-', '.', '/', '_'
+        P2_TEST_VALID = b'..--//__0x'   # valid ranges: 'A' - 'Z', 'a' - 'z', '0' - '9', '-', '.', '/', '_'
+        a2 = A2Packet()
+        a2c = A2Packet()
+
+        a2.data.Count = 2
+        a2.create_opt_fields(prot_count=2)
+        a2.Prot[0].P1 = util.cbytes(A2Packet.SC2_PROT_STRING)
+        a2.Prot[0].P2 = util.cbytes(A2Packet.UNSPECIFIED_PROT_STRING)
+        a2.Prot[1].P1 = util.cbytes(P1_TEST_VALID)
+        a2.Prot[1].P2 = util.cbytes(P2_TEST_VALID)
+
+        self.assertEqual(bytes(a2.Prot[0].P1), A2Packet.SC2_PROT_STRING)
+        self.assertEqual(bytes(a2.Prot[0].P2), A2Packet.UNSPECIFIED_PROT_STRING)
+        self.assertEqual(bytes(a2.Prot[1].P1), P1_TEST_VALID)
+        self.assertEqual(bytes(a2.Prot[1].P2), P2_TEST_VALID)
+
+        a2c.from_bytes(src=bytes(a2))
+        self.assertEqual(bytes(a2), bytes(a2c))
+        self.assertEqual(a2c.data.Header.NoSuchServer, 0)
+        self.assertEqual(a2c.data.Header.LastFlag, 1)
+        self.assertEqual(a2c.data.Count, 2)
+        self.assertTrue(a2c.opt.Prot)
+        self.assertEqual(bytes(a2c.Prot[0].P1), A2Packet.SC2_PROT_STRING)
+        self.assertEqual(bytes(a2c.Prot[0].P2), A2Packet.UNSPECIFIED_PROT_STRING)
+        self.assertEqual(bytes(a2c.Prot[1].P1), P1_TEST_VALID)
+        self.assertEqual(bytes(a2c.Prot[1].P2), P2_TEST_VALID)
+
+    def test_A2_prop_invalid2(self):
+        a2 = A2Packet()
+        a2c = A2Packet()
+
+        a2.data.Count = 1
+        a2.create_opt_fields(prot_count=1)
+        a2.Prot[0].P1 = util.cbytes(b':+@)(`#^&*')
+        a2.Prot[0].P2 = util.cbytes(b'_ _\t><=~12')
+        with self.assertRaises(BadPeer) as cm:
+            a2c.from_bytes(src=bytes(a2))
 
 
 
